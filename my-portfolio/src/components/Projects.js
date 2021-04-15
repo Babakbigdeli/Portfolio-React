664edc5144d918852f001c454f9845d4df4cff21
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-15 17:53:55 +0100

    Projects component added

---
+++ my-portfolio/src/components/Projects.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import Card from 'react-bootstrap/Card'
+
+
+
+
+
+class Projects extends Component {
+    render() {
+    return (
+      <>
+  <Card bsPrefix='card' border="warning" >
+    <Card.Img src={this.props.image} />
+     <Card.Body>
+         <Card.Title as="h2">
+            {this.props.title}
+         </Card.Title>
+         <Card.Text as="h6">
+            {this.props.description}
+         </Card.Text>
+     </Card.Body>
+     <Card.Footer as="h5">
+           <a href={this.props.githubLink} className="card-link">
+              Github Link
+            </a>
+            <br></br>
+            <a href={this.props.deployedLink}className="card-link">
+              Deployed Link
+            </a>
+    </Card.Footer>
+  </Card>
+        </>
+    );
+    }
+    }
+    export default Projects;
+ 
+
