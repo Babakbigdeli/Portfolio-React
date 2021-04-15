d964ce3068a2aea50fa43f25838fda8cea1381dd
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-15 23:46:30 +0100

    contents added

--- src/contents/About.js
+++ src/contents/About.js
@@ -14,11 +14,13 @@ return (
 <ReactTypingEffect className="typingeffect" text={['I am Babak Bigdeli','I am a full stack web developer']} speed={100} eraseDelay={400}/>
 <br></br>
 <p>45, living in Greater Manchester, a full stack Web Developer graduated from Manchester University BootCamp. 
-      My passion for coding started with finding and tweaking VBA codes to use in Excel macros. 
-     I am a good team player who uses every oppurtunity to learn new concepts and keep my knowledge up to date.
+      My passion for coding started with finding and tweaking VBA codes to use in Excel macros. After nearly 20 years of working in logistics industry, 
+      I decided to learn a new skill and change my line of work mainly because I felt there is not much more left for me to learn. And what really excites 
+      me about this new skill is the fact that I have just stepped in a world that requires constant learning and keeping up to date and there
+      is definitely enough stuff for me to learn in this field for the rest of my life.
 </p>
-<p>I got avid interest in technology and computers, passion for music, reading, films and cooking/baking. 
-    Practising meditation, travelling, road trips, wild camping, spending quality time with my family and my friends.
+<p>I got avid interest in technology and computers, passion for music, reading, films and cooking/baking.
+    Practising meditation, travelling, road trips, camping, spending quality time with my family and friends.
 </p>
 <br></br>
 <SocialFooter />
@@ -26,4 +28,4 @@ return (
 )
 }
 }
-export default About
+export default About;
