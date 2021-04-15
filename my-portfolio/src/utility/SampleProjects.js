31c1027cff86a423077caf2f4c54f2b878481cc0
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-15 17:58:37 +0100

    Seeder data added

---
+++ my-portfolio/src/utility/SampleProjects.js
@@ -0,0 +1,56 @@
+import DayPlanner from  "../image/DayPlanner.jpg";
+import EmpDirectory from  "../image/EmpDirectory.jpg";
+import FriendNightIn from  "../image/FriendNightIn.jpg";
+import Pantry from  "../image/Pantry.jpg";
+import WeatherDash from  "../image/WeatherDash.jpg";
+import WorkoutTracker from  "../image/WorkoutTracker.jpg";
+
+
+
+let SampleProjects = [
+
+  {
+    title: "Day-Planner",
+    description: " A simple calendar app for scheduling your work day ",
+    githubLink: "https://github.com/Babakbigdeli/Day-Planner",
+    deployedLink: "https://babakbigdeli.github.io/Day-Planner/",
+    image: DayPlanner
+  },
+  {
+    title: "Employee-Directory-React",
+    description: " An online application that helps an employer to see non sensitive information of the employees in form of a directory",
+    githubLink: "https://github.com/Babakbigdeli/Employee-Directory-React",
+    deployedLink: "https://babakbigdeli.github.io/Employee-Directory-React/",
+    image: EmpDirectory
+  },
+  {
+    title: "Friends-night-in",
+    description: " A web application that helps you choose the perfect food and movie combination",
+    githubLink: "https://github.com/Babakbigdeli/friends-night-in",
+    deployedLink: "https://babakbigdeli.github.io/friends-night-in/",
+    image: FriendNightIn
+  },
+  {
+    title: "The-Pantry",
+    description: " An app to help keep track of food in your kitchen and track waste",
+    githubLink: "https://github.com/Babakbigdeli/Project-2-The-Pantry",
+    deployedLink: "http://pantrycheck.herokuapp.com/",
+    image: Pantry
+  },
+  {
+    title: "Weather-Dashboard",
+    description: "This applicaiton is a weather dashboard with search functionality to find current weather conditions and the future weather outlook for multiple cities. This application utilizes the AJAX to hook into OpenWeather API to retrieve data in JSON format. Using dynamically updated HTML and CSS powered by jQuery",
+    githubLink: "https://github.com/Babakbigdeli/Weather-Dashboard",
+    deployedLink: "https://babakbigdeli.github.io/Weather-Dashboard/",
+    image: WeatherDash
+  },
+  {
+    title: "Workout-Tracker",
+    description: "A web based application with a Mongodb database that helps the user to keep track of its daily workout (both Cardio and Resistance) and reports it on a chart",
+    githubLink: "https://github.com/Babakbigdeli/Workout-Tracker",
+    deployedLink: "https://thawing-everglades-97729.herokuapp.com/into",
+    image: WorkoutTracker
+  }
+];
+
+export default SampleProjects;
