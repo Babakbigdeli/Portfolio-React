66cc7711c2bdc38f1586ad232f4dfa6eae18acbf
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-15 17:57:04 +0100

    contact page added

---
+++ my-portfolio/src/contents/Contact.js
@@ -0,0 +1,23 @@
+import React, { Component, Fragment } from 'react';
+import SocialFooter from '../components/SocialFooter';
+
+
+class Contact extends Component {
+render() {
+return (
+<div className="condiv">
+<h1>Contact Me</h1>
+<h3>Email  :   babakbigdeli@gmail.com</h3>
+<h3>Phone   :   07456184955</h3>
+<br></br>
+<h3>Please download my CV < a href="Assets/MyWebDevCV.docx" >here</a></h3>
+
+<>
+<SocialFooter />
+</>
+</div>
+
+)
+}
+}
+export default Contact;
