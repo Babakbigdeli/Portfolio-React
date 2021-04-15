e103e9f9f2113a18b97adc7b33c777387950bf31
Babak Bigdeli <babakbigdeli@gmail.com>
2021-04-16 00:20:34 +0100

    Contents updated

--- src/utility/SampleProjects.js
+++ src/utility/SampleProjects.js
@@ -11,42 +11,42 @@ let SampleProjects = [
 
   {
     title: "Day-Planner",
-    description: " A simple calendar app for scheduling your work day ",
+    description: " A simple calendar app for scheduling your work day. HTML, CSS, Javascript, jQuery, Moment.js and Bootstrap was used to make this application,",
     githubLink: "https://github.com/Babakbigdeli/Day-Planner",
     deployedLink: "https://babakbigdeli.github.io/Day-Planner/",
     image: DayPlanner
   },
   {
     title: "Employee-Directory-React",
-    description: " An online application that helps an employer to see non sensitive information of the employees in form of a directory",
+    description: " An online application that helps an employer to see non sensitive information of the employees in form of a directory. Javascript, REACT, CSS, HTML, NodeJs iwas used in making this appliction",
     githubLink: "https://github.com/Babakbigdeli/Employee-Directory-React",
     deployedLink: "https://babakbigdeli.github.io/Employee-Directory-React/",
     image: EmpDirectory
   },
   {
     title: "Friends-night-in",
-    description: " A web application that helps you choose the perfect food and movie combination",
+    description: " A web application that helps you choose the perfect food and movie combination. Javascript, HTML, CSS, Bulma, Jquery was used to make this application",
     githubLink: "https://github.com/Babakbigdeli/friends-night-in",
     deployedLink: "https://babakbigdeli.github.io/friends-night-in/",
     image: FriendNightIn
   },
   {
     title: "The-Pantry",
-    description: " An app to help keep track of food in your kitchen and track waste",
+    description: " An app to help keep track of food in your kitchen and track waste. Javascript, HTML, CSS, Heroku, Handlebars, MySQL, Sequelize, Express, Node and MVC Paradigm was used to build this application.",
     githubLink: "https://github.com/Babakbigdeli/Project-2-The-Pantry",
     deployedLink: "http://pantrycheck.herokuapp.com/",
     image: Pantry
   },
   {
     title: "Weather-Dashboard",
-    description: "This applicaiton is a weather dashboard with search functionality to find current weather conditions and the future weather outlook for multiple cities. This application utilizes the AJAX to hook into OpenWeather API to retrieve data in JSON format. Using dynamically updated HTML and CSS powered by jQuery",
+    description: "This applicaiton is a weather dashboard with search functionality to find current weather conditions and the future weather outlook for multiple cities. This application utilizes the AJAX to hook into OpenWeather API to retrieve data in JSON format. Javascript, HTML, CSS, Jquery was used to build this application.",
     githubLink: "https://github.com/Babakbigdeli/Weather-Dashboard",
     deployedLink: "https://babakbigdeli.github.io/Weather-Dashboard/",
     image: WeatherDash
   },
   {
     title: "Workout-Tracker",
-    description: "A web based application with a Mongodb database that helps the user to keep track of its daily workout (both Cardio and Resistance) and reports it on a chart",
+    description: "A web based application with a Mongodb database that helps the user to keep track of its daily workout (both Cardio and Resistance) and reports it on a chart. Javascript, HTML, CSS, Mongodb, Mongoose, Heroku, Express and NodeJs was used to make this application.",
     githubLink: "https://github.com/Babakbigdeli/Workout-Tracker",
     deployedLink: "https://thawing-everglades-97729.herokuapp.com/into",
     image: WorkoutTracker
