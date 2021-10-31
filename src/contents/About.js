783d0dc10286d9c9c5e0cbe25b24b5e707e077fd
Babak <babakbigdeli@gmail.com>
2021-10-31 14:16:53 +0000

    about page updated

--- src/contents/About.js
+++ src/contents/About.js
@@ -10,19 +10,18 @@ return (
 < div className="condiv home">
 <img src={profilepic} className="profilepic"></img>
 <br></br>
-<ReactTypingEffect className="typingeffect" text={['I am Babak Bigdeli','I am a full stack web developer']} speed={100} eraseDelay={400}/>
+<ReactTypingEffect className="typingeffect" text={['My name is Babak Bigdeli','I am a full stack web developer']} speed={100} eraseDelay={400}/>
 <br></br>
-<p>Living in Greater Manchester, I am a Full-Stack Web Developer graduated from the University of Manchester's Bootcamp.
-      My passion for coding initially started with finding and tweaking VBA codes to use in MS Access and Excel. After so many years of working in Admin and Logistics industry, 
-      I decided to learn a new skill and change my line of work mainly because I felt there is not much more left in that business for me to learn. Therefore I enrolled 
-      in the University of Manchester's Full-Stack Bootcamp which was a 24-week intensive programme focused on teaching programming skills in 
-      HTML5, CSS3, JavaScript, jQuery, Bootstrap, Express, Handelbars, Firebase, NodeJS, MySQL, MongoDB,  and ReactJS. Here I acquired a sound understanding of programming, 
+<p>Living in Greater Manchester, I graduated from the University of Manchester's Bootcamp.
+      Having developed a passion for coding which evolved after finding and tweaking VBA codes to use in MS Access and Excel. I spent many years working in the Admin and 
+      logistics industry until I decided to learn a new skill and change my line of work because I felt the need to expand my horizons. 
+      The bootcamp was a 24-week intensive programme focused on teaching programming skills in 
+      JavaScript, jQuery, HTML5, CSS3, Bootstrap, NodeJS, MySQL, MongoDB, ReactJs, Express, Handelbars and Firebase.  This enabled me to acquire a sound understanding of programming, 
       with a focus on test-driven development and collaborative version control with Github. I gained experience with backend technologies, building APIs and servers with Express and 
-      MySql, whilst also covering the front-end with React and CSS. And that is just the start! What really excites me is the fact that I have just stepped into a world that demands constant 
-      learning and keeping my knowledge up to date  probably for the rest of my life 😀
+      MySql, whilst also covering the front-end with React and CSS. This is just the start of my journey and I am excited to be in a world that demands continuous learning and development. 
+      I look forward to keeping up with the evolving knowledge for the rest of my career.
 </p>
-<p>I got avid interest in technology and computers, passion for music, reading, films and cooking/baking.
-    Practising meditation, travelling, road trips, camping and spending quality time with my family and friends.
+<p> I also have an avid interest in computers and technology in general, passion for music, reading, films and cooking. I tend to balance my life and everyday stresses by practising  meditation, travelling etc.
 </p>
 </div>
 )
