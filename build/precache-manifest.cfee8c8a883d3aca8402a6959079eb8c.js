f15324edc40c8ff90cb6b689e5955a8293ebbd16
Babak <babakbigdeli@gmail.com>
2021-11-02 11:38:14 +0000

    build updated

---
+++ build/precache-manifest.cfee8c8a883d3aca8402a6959079eb8c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214bb386e4c96095d8ea830f2cc182b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddab63d6dfe5fd16a3d9",
+    "url": "/static/css/main.8d581568.chunk.css"
+  },
+  {
+    "revision": "dd52e207fa87dd45ff21",
+    "url": "/static/js/2.e11c364c.chunk.js"
+  },
+  {
+    "revision": "7dde62080d6af64616a3ed8563a0d758",
+    "url": "/static/js/2.e11c364c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddab63d6dfe5fd16a3d9",
+    "url": "/static/js/main.a470b679.chunk.js"
+  },
+  {
+    "revision": "8aba14c5515e5362888a",
+    "url": "/static/js/runtime-main.66cf0057.js"
+  },
+  {
+    "revision": "6e232373bbe5bb761e3e84b0255ada58",
+    "url": "/static/media/EmpDirectory.6e232373.jpg"
+  },
+  {
+    "revision": "9b8c5fef1a69611aaa2c63dd665979aa",
+    "url": "/static/media/FriendNightIn.9b8c5fef.jpg"
+  },
+  {
+    "revision": "c5b6025bc47ecaebe72a46b37f42ffd1",
+    "url": "/static/media/GoogleBooks1.c5b6025b.jpg"
+  },
+  {
+    "revision": "442905f18b3d08761cfeb3f8c2eb7b32",
+    "url": "/static/media/Me.442905f1.jpg"
+  },
+  {
+    "revision": "6b94971f7941bb52287e12888b3b15c7",
+    "url": "/static/media/Pantry.6b94971f.jpg"
+  },
+  {
+    "revision": "3bfb37a8944295ea9ea3538ae218d562",
+    "url": "/static/media/WeatherDash.3bfb37a8.jpg"
+  },
+  {
+    "revision": "a919a54cc46c48725146415207f12661",
+    "url": "/static/media/WorkoutTracker.a919a54c.jpg"
+  }
+]);
