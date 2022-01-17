d99e46a8357e9e7f2a670522c6e5e3a867284b01
babak bigdeli <babakbigdeli@gmial.com>
2022-01-17 12:46:34 +0000

    Minir modifications in codebase

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -9,8 +9,7 @@ return (
 <h3>Email  :   babakbigdeli@gmail.com</h3>
 <h3>Phone   :   07456184955</h3>
 <br></br>
-{/*eslint-disable-next-line*/}
-<h3>Please download my CV <a href="#" id="CV-Link" onClick={()=> window.open("https://docs.google.com/document/d/1DyQjN9hKZ-muCzeaDVhlilFg_Y2tNBTPvp52c6zfPos/edit?usp=sharing")}>here </a>(opens in new tab)</h3>
+<h3>Please visit my CV <a href="https://docs.google.com/document/d/1DyQjN9hKZ-muCzeaDVhlilFg_Y2tNBTPvp52c6zfPos/edit?usp=sharing" id="CV-Link" target="_blank" rel="noopener noreferrer">here </a>(opens in a new tab)</h3>
 <>
 
 </>
