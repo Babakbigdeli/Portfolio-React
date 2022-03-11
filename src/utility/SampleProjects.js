d44717d078e6987610a4ad3437a6423107c16e9b
babak bigdeli <babakbigdeli@gmial.com>
2022-03-11 13:40:53 +0000

    more project samples added

--- src/utility/SampleProjects.js
+++ src/utility/SampleProjects.js
@@ -4,6 +4,8 @@ import FriendNightIn from  "../image/FriendNightIn.jpg";
 import Pantry from  "../image/Pantry.jpg";
 import WeatherDash from  "../image/WeatherDash.jpg";
 import WorkoutTracker from  "../image/WorkoutTracker.jpg";
+import Bookmark from  "../image/Bookmark.jpg";
+import DayScheduler from  "../image/DayScheduler.jpg";
 
 
 
@@ -18,14 +20,14 @@ let SampleProjects = [
   },
   {
     title: "Employee-Directory-React",
-    description: " An online application that helps an employer to see non sensitive information of the employees in form of a directory. Javascript, REACT, CSS, HTML, NodeJs was used in making this appliction",
+    description: " An application that helps employers to see non sensitive information of the employees in form of a directory. Javascript, REACT, CSS, HTML, NodeJs was used in making this appliction",
     githubLink: "https://github.com/Babakbigdeli/Employee-Directory-React",
     deployedLink: "https://babakbigdeli.github.io/Employee-Directory-React/",
     image: EmpDirectory
   },
   {
     title: "Friends-night-in",
-    description: " A web application that helps you choose the perfect food and movie combination. Javascript, HTML, CSS, Bulma, Jquery was used to make this application",
+    description: " A web application that helps user to choose the perfect food and movie combination. Javascript, HTML, CSS, API calls, Bulma, Jquery was used to make this application",
     githubLink: "https://github.com/Babakbigdeli/friends-night-in",
     deployedLink: "https://babakbigdeli.github.io/friends-night-in/",
     image: FriendNightIn
@@ -46,10 +48,24 @@ let SampleProjects = [
   },
   {
     title: "Workout-Tracker",
-    description: "A web based application with a Mongodb database that helps the user to keep track of its daily workout (both Cardio and Resistance) and reports it on a chart. Javascript, HTML, CSS, Mongodb, Mongoose, Heroku, Express and NodeJs was used to make this application.",
+    description: "A web based application with a Mongodb database that helps the user to keep track of their daily workout (both Cardio and Resistance) and rshows it on a chart. Javascript, HTML, CSS, Mongodb, Mongoose, Heroku, Express and NodeJs was used to make this application.",
     githubLink: "https://github.com/Babakbigdeli/Workout-Tracker",
     deployedLink: "https://thawing-everglades-97729.herokuapp.com",
     image: WorkoutTracker
+  },
+  {
+    title: "Book-Mark",
+    description: "BookMark is an app that utilizes a React frontend, a NodeJs and Express backend and a mongoDB database. It uses Auth0 for authentication and GoogleBooks and Guardian API calls. Users can manage their readings by saving the titles in the database and then moving it between different categories as they progress in reading it.",
+    githubLink: "https://github.com/Babakbigdeli/BookmarkApp",
+    deployedLink: "https://digital-bookmark.herokuapp.com/",
+    image: Bookmark
+  },
+  {
+    title: "Day-Planner",
+    description: "A simple browser based work scheduler app where users can shcedule their daily tasks from 9am to 5pm. It utilizes local storage to save users notes and color-codes each line based on waht time of the day it is into past, present and future. ",
+    githubLink: "https://github.com/Babakbigdeli/Day-Planner",
+    deployedLink: "https://babakbigdeli.github.io/Day-Planner/",
+    image: DayScheduler
   }
 ];
 
