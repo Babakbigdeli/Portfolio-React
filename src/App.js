c1403f65d3da82371e52afaf6aebbaa8ebcee03b
Babak <babakbigdeli@gmail.com>
2025-03-04 18:24:32 +0000

    React Router v6 parameters added. bugs in code fixed. styling fixes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router, Route,} from "react-router-dom";
-import Navbar from './components/Navbar';
-import Contact from './contents/Contact';
-import About from './contents/About';
-import Portfolio from './contents/Portfolio';
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Contact from "./contents/Contact";
+import About from "./contents/About";
+import Portfolio from "./contents/Portfolio";
 
 function App() {
-return (
-<Router>
-<div className="App">
-<Navbar />
-<Route exact path="/"><About />
-</Route>
-<Route path="/about"><About />
-</Route>
-<Route path="/portfolio"><Portfolio />
-</Route>
-<Route path="/contact"><Contact />
-</Route>
-</div>
-</Router>
-)
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-100 text-gray-900">
+        <Navbar />
+        <div className="container mx-auto p-6">
+          <Routes>
+            <Route path="/" element={<About />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
 }
+
 export default App;
