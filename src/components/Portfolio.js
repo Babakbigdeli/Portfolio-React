d2fff8f5a2a5dee93f276ee29e6a33fa64751487
Babak <babakbigdeli@gmail.com>
2025-03-06 14:58:49 +0000

    Code improvement

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,14 +6,14 @@ function Portfolio() {
     <div className="mt-16 p-6 bg-white shadow-md rounded-lg">
       <h2 className="text-2xl font-bold mb-4">My Projects</h2>
       <div className="grid md:grid-cols-2 gap-6">
-        {SampleProjects.map((project, index) => (
+        {SampleProjects.map(({ image, title, description, githubLink, deployedLink }, index) => (
           <Projects
             key={index}
-            image={project.image}
-            title={project.title}
-            description={project.description}
-            githubLink={project.githubLink}
-            deployedLink={project.deployedLink}
+            image={image}
+            title={title}
+            description={description}
+            githubLink={githubLink}
+            deployedLink={deployedLink}
           />
         ))}
       </div>
