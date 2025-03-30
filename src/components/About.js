99640a4517bd7902b0e25069cb23cd77f06bcaa8
Babak Bigdeli <babakbigdeli@gmail.com>
2025-03-30 20:24:20 +0100

    Content update

--- src/components/About.js
+++ src/components/About.js
@@ -2,30 +2,29 @@ function About() {
       return (
         <div className="mt-16 p-6 bg-[#e9e5dc] shadow-md rounded-lg">
           <h2 className="text-2xl font-bold">About Me</h2>
-          <p className="mt-4">Over 15 years in logistics and administration at major firms such as Total 
-Energies, Aramex, DHL, K&N, and JD Sports introduced me to advanced use of 
-Microsoft Office, especially for process optimization. That exposure sparked a 
-growing interest in coding with VBA and SQL, enabling me to design custom 
-spreadsheets, automate workflows, and manage databases more efficiently. During 
-the COVID-19 pandemic, I recognized a unique chance to pivot into tech, 
-enrolling in a rigorous 24-week MERN bootcamp at Manchester University to 
-expand my programming capabilities. 
-A few month later, I started worked as a Shopify Developer with Open Supplies, 
-where I focused on enhancing user experiences and improving the overall 
-functionality of multiple e-commerce platforms. Eager to tackle more complex 
-challenges, I then joined Intellithing, collaborating on the creation of an AI- 
-focused web portal using Scrum principles and GitHub for version control. 
-Between sprints, I supported digital marketing initiatives, leveraging LinkedIn 
-Sales Navigator and Pipedrive to help drive growth and engagement. 
- 
-I continuously sharpen my development skills through coding challenges, stay 
-informed via tech news, and explore Generative AI to uncover innovative 
-solutions—learning from it (or perhaps learning with it!).Drawing on my 
-extensive administrative background and problem-solving drive, I bring a 
-distinctive perspective to any project. I look forward to applying my 
-adaptability, creativity, and strong work ethic in a new role, where I can 
-contribute not only to technical development but also to the broader goals and 
-success of the organization. </p>
+          <p className="mt-4">Over 15 years in logistics and administration at major multinational 
+              firms introduced me to the advanced use of Microsoft Office, particularly for process optimization. 
+              That exposure sparked a growing interest in coding with VBA and SQL, enabling me to design custom 
+              spreadsheets, automate workflows, and manage databases more efficiently. During 
+              the COVID-19 pandemic, I recognized a unique chance to pivot into tech, 
+              enrolling in a rigorous 24-week MERN bootcamp at Manchester University to 
+              expand my programming capabilities. 
+              A few month later, I started worked as a Shopify Developer with Open Supplies, 
+              where I focused on enhancing user experiences and improving the overall 
+              functionality of multiple e-commerce platforms. Eager to tackle more complex 
+              challenges, I then joined Intellithing, collaborating on the creation of an AI- 
+              focused web portal using Scrum principles and GitHub for version control. 
+              Between sprints, I supported digital marketing initiatives, leveraging LinkedIn 
+              Sales Navigator and Pipedrive to help drive growth and engagement. 
+              
+              I continuously sharpen my development skills through coding challenges, stay 
+              informed via tech news, and explore Generative AI to uncover innovative 
+              solutions—learning from it (or perhaps learning with it!).Drawing on my 
+              extensive administrative background and problem-solving drive, I bring a 
+              distinctive perspective to any project. I look forward to applying my 
+              adaptability, creativity, and strong work ethic in a new role, where I can 
+              contribute not only to technical development but also to the broader goals and 
+              success of the organization. </p>
         </div>
       );
     }
